9f80abde7239cf5ae7bbab06fe9d885e8be4f15d
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-06-13 23:57:44 +0200

    Three helper methods

---
+++ JavaScript-tutorial/app.js
@@ -0,0 +1,9 @@
+// Three helper methods:
+
+document.write('Hello, World :)');
+
+alert('Hello, people 👋');
+
+console.log('hello, console!');
+// Also works with objects:
+console.log({ name: 'Tronadís' });
