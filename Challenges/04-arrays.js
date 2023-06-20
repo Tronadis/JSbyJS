7d8f818c1558c51ab68d18a11e07e4b1aa8abf65
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-06-20 03:04:58 +0200

    04-arrays challenge done

---
+++ Challenges/04-arrays.js
@@ -0,0 +1,11 @@
+// 1. create "fruits" array and store some fruit values
+// 2. setup the last item as number (random)
+// 3. assign first fruit to the variable
+// 4. re-assign last array item to the actual fruit
+// 5. log both first fruit variable and entire fruits array
+
+const fruits = ['apple', 'pear', 'apricot', 'banana', 'orange', 545];
+const firstFruit = fruits[0];
+fruits[5] = 'tangerine';
+console.log(firstFruit);
+console.log(fruits);
