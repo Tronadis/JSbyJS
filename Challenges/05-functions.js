714716c5195569ad6201e351e64d198c6d5478a0
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-06-20 22:07:58 +0200

    05-functions.js challenge done

--- Challenges/05-functions.js
+++ Challenges/05-functions.js
@@ -19,3 +19,31 @@ function greet(name) {
 greet('Bob');
 greet('Anna');
 greet('Suzy');
+
+// The challenge:
+
+// 1. create "calculateTotal" function
+// 2. add two parameters subTotal, tax
+// 3. return sum of parameters
+// 4. create 3 vars "order1","order2","order3"
+// 5. call calculateResult, pass in some values and assign result to each order
+// 6. log all three orders
+// 7. refactor "calculateTotal" to function expression
+
+// With function declaration:
+function calculateTotal(subTotal, tax) {
+	return subTotal + tax;
+}
+let order1 = calculateTotal(200, 20);
+let order2 = calculateTotal(300, 30);
+let order3 = calculateTotal(400, 40);
+console.log(order1, order2, order3);
+
+// The same, with function expression:
+const calculateResult = function (subTotal, tax) {
+	return subTotal + tax;
+};
+let order4 = calculateResult(200, 20);
+let order5 = calculateResult(300, 30);
+let order6 = calculateResult(400, 40);
+console.log(order4, order5, order6);
