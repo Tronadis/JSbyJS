8d1b2838475e5184536e41f8b512c339f42bda4f
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-06-25 01:00:37 +0200

    08-strings challenge done

--- Challenges/08-strings.js
+++ Challenges/08-strings.js
@@ -5,3 +5,28 @@
 // 5. log result
 // 6. change the order of arguments
 // 7. refactor to object parameter
+
+// me:
+function fullName(firstName, lastName) {
+	console.log(firstName + ' ' + lastName.toUpperCase());
+	console.log(`${firstName} ${lastName.toUpperCase()}`);
+	console.log(`${lastName.toUpperCase()} ${firstName}`);
+}
+fullName('John', 'Doe');
+fullName('Jane', 'Dee');
+
+// teacher:
+function fullName2(firstName, lastName) {
+	const fullName = `${firstName} ${lastName}`;
+	return fullName.toUpperCase();
+}
+console.log(fullName2('John', 'Smith'));
+// also
+function fullName3({ firstName, lastName }) {
+	// function params as object params
+	const fullName = `${firstName} ${lastName}`;
+	return fullName.toUpperCase();
+}
+console.log(fullName3({ firstName: 'Peter', lastName: 'Jordan' }));
+console.log(fullName3({ lastName: 'Jordan', firstName: 'Peter' }));
+// (So the idea was to decare 2nd value first, not to change the order in output!)
