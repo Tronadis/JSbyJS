8813e4e3175f57ba137135a528c31370437a3f6e
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-07-02 09:39:59 +0200

    Temp back to single js file (until fix)

--- Challenges/09-array-methods.js
+++ Challenges/09-array-methods.js
@@ -47,38 +47,38 @@
 //    }
 // 2. assign to survey and log
 
-// const students = [
-// 	{
-// 		id: 1,
-// 		name: 'Peter',
-// 		score: 80,
-// 		favoriteSubject: 'math',
-// 	},
-// 	{
-// 		id: 2,
-// 		name: 'Sarah',
-// 		score: 90,
-// 		favoriteSubject: 'math',
-// 	},
-// 	{
-// 		id: 3,
-// 		name: 'Amadou',
-// 		score: 85,
-// 		favoriteSubject: 'history',
-// 	},
-// 	{
-// 		id: 4,
-// 		name: 'Poonam',
-// 		score: 92,
-// 		favoriteSubject: 'math',
-// 	},
-// 	{
-// 		id: 5,
-// 		name: 'Andrzei',
-// 		score: 87,
-// 		favoriteSubject: 'art',
-// 	},
-// ];
-// 'studients' array moved to data.js
+const students = [
+	{
+		id: 1,
+		name: 'Peter',
+		score: 80,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 2,
+		name: 'Sarah',
+		score: 90,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 3,
+		name: 'Amadou',
+		score: 85,
+		favoriteSubject: 'history',
+	},
+	{
+		id: 4,
+		name: 'Poonam',
+		score: 92,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 5,
+		name: 'Andrzei',
+		score: 87,
+		favoriteSubject: 'art',
+	},
+];
+// 'studients' array moved to data.js (but temp kept here, until fix)
 
 console.log(students);
