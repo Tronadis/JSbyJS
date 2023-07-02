b41a370aef19c4201f04b96226df6b7f182744a5
Tronadis <89267750+Tronadis@users.noreply.github.com>
2023-07-02 09:36:10 +0200

    Array methods challenges setup
    But it doesn't work. Need explanation.

--- Challenges/09-array-methods.js
+++ Challenges/09-array-methods.js
@@ -46,3 +46,39 @@
 //    math: 3
 //    }
 // 2. assign to survey and log
+
+// const students = [
+// 	{
+// 		id: 1,
+// 		name: 'Peter',
+// 		score: 80,
+// 		favoriteSubject: 'math',
+// 	},
+// 	{
+// 		id: 2,
+// 		name: 'Sarah',
+// 		score: 90,
+// 		favoriteSubject: 'math',
+// 	},
+// 	{
+// 		id: 3,
+// 		name: 'Amadou',
+// 		score: 85,
+// 		favoriteSubject: 'history',
+// 	},
+// 	{
+// 		id: 4,
+// 		name: 'Poonam',
+// 		score: 92,
+// 		favoriteSubject: 'math',
+// 	},
+// 	{
+// 		id: 5,
+// 		name: 'Andrzei',
+// 		score: 87,
+// 		favoriteSubject: 'art',
+// 	},
+// ];
+// 'studients' array moved to data.js
+
+console.log(students);
---
+++ Challenges/10-data.js
@@ -0,0 +1,32 @@
+const students = [
+	{
+		id: 1,
+		name: 'Peter',
+		score: 80,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 2,
+		name: 'Sarah',
+		score: 90,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 3,
+		name: 'Amadou',
+		score: 85,
+		favoriteSubject: 'history',
+	},
+	{
+		id: 4,
+		name: 'Poonam',
+		score: 92,
+		favoriteSubject: 'math',
+	},
+	{
+		id: 5,
+		name: 'Andrzei',
+		score: 87,
+		favoriteSubject: 'art',
+	},
+];
