66db44c1f67d27a201e683beee9f1e32d47e6950
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-04-04 23:24:00 +0200

    Toggle dropdown using DOM navigation.

---
+++ Projects/07-questions/app.js
@@ -0,0 +1,10 @@
+// Method 1: traversing the DOM
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		const question = e.currentTarget.parentElement.parentElement;
+		question.classList.toggle('show-text');
+	});
+});
