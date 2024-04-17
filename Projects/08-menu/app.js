d5a1698f54a1ed4706ff5d20ab6a350071be6b8a
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-04-17 19:34:34 +0200

    Filter buttons (but broken)

--- Projects/08-menu/app.js
+++ Projects/08-menu/app.js
@@ -73,10 +73,30 @@ const menu = [
 	},
 ];
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 window.addEventListener('DOMContentLoaded', function () {
 	displayMenuItems(menu);
 });
+
+filterBtns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		const category = e.currentTarget.dataset;
+		const menuCategory = menu.filter(function (menuItem) {
+			// console.log(menuItem.category);
+			if (menuItem.category === category) {
+				return menuItem;
+			}
+		});
+		// console.log(menuCategory)
+		if (menuItem === category) {
+			displayMenuItems(menu);
+		} else {
+			displayMenuItems(menuCategory);
+		}
+	});
+});
+
 function displayMenuItems(menuItems) {
 	let displayMenu = menuItems.map(function (item) {
 		return `<article class="menu-item">
