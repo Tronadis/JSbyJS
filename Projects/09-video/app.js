a609c6cefe0a597e7050f3a8e1929ce505907dc7
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-04-27 20:58:20 +0200

    Video: add preloader

--- Projects/09-video/app.js
+++ Projects/09-video/app.js
@@ -19,3 +19,8 @@ btn.addEventListener('click', function () {
 		video.play();
 	}
 });
+
+const preloader = document.querySelector('.preloader');
+window.addEventListener('load', function () {
+	preloader.classList.add('hide-preloader');
+});
