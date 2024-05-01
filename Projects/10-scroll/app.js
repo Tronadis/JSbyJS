684f94d7b0cb9f67b8f3e648c655517629648016
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-05-01 11:27:12 +0200

    Scroll: toggle nav (with dynamic height)

--- Projects/10-scroll/app.js
+++ Projects/10-scroll/app.js
@@ -7,7 +7,20 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', function () {
+	// linksContainer.classList.toggle('show-links'); // Works with height hardcoded in stylesheet; limited interest.
+	const containerHeight = linksContainer.getBoundingClientRect().height;
+	const linksHeight = links.getBoundingClientRect().height;
+	if (containerHeight === 0) {
+		linksContainer.style.height = `${linksHeight}px`;
+	} else {
+		linksContainer.style.height = `0px`;
+	}
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
