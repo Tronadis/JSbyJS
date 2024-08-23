c5c54c513aceebfb3dfc73f8649182853c1488e7
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-08-23 20:17:03 +0200

    Countdown: set deadline

--- Projects/12-countdown/app.js
+++ Projects/12-countdown/app.js
@@ -21,3 +21,20 @@ const months = [
     "Friday",
     "Saturday",
   ];
+
+  const giveaway = document.querySelector('.giveaway');
+  const deadline = document.querySelector('.deadline');
+  const timeLevel = document.querySelectorAll('.deadline-format h4');
+
+  let futureDate = new Date(2024,8,7,0);
+
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
+  // let month = futureDate.getMonth(); month = months[month];
+  const month = months[futureDate.getMonth()];
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year}, ${hours}:${minutes}`;
+
