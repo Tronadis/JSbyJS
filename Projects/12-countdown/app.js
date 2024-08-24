fdeb2afadfb847a6a13e7ab909602ff93b8973b1
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-08-25 00:21:06 +0200

    countdown display and automation

--- Projects/12-countdown/app.js
+++ Projects/12-countdown/app.js
@@ -38,3 +38,36 @@ const months = [
 
 giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year}, ${hours}:${minutes}`;
 
+const futurTime = futureDate.getTime();
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const T = futurTime - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  const oneSecond = 1000;
+
+  // let day = T / oneDay; day = Math.floor(day);
+  const day = Math.floor(T / oneDay);
+  let hours = Math.floor((T % oneDay) / oneHour);
+  let minutes = Math.floor((T % oneHour) / oneMinute);
+  let seconds = Math.floor((T % oneMinute) / oneSecond);
+
+  const timeValues = [day, hours, minutes, seconds];
+  function format(item){
+    if(item < 10) {
+      return `0${item}`;
+    }
+    return item;
+  }
+  timeLevel.forEach(function (level, index) {
+    level.innerHTML = format(timeValues[index]);
+  });
+  if (T < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired<h4>`;
+  }
+}
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
