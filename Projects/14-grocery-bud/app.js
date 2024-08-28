1aa214e6c1829385429b376d1e02f945ec995c4a
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-08-28 20:43:26 +0200

    addItem function

--- Projects/14-grocery-bud/app.js
+++ Projects/14-grocery-bud/app.js
@@ -12,9 +12,59 @@ let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+function addItem (e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString(); // Just a hack to get a unique string locally. Not for IRL.
+    if(value && !editFlag) {
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `
+                <p class="title">${value}</p>
+                <div class="btn-container">
+                    <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+                    <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>
+                </div>
+        `;
+        list.appendChild(element);
+        displayAlert(`${value} added to the list`, "success");
+        container.classList.add("show-container");
+        addToLocalStorage(id,value);
+        setBackToDefault();
+        
+    } else if(value && editFlag) {
+        console.log("editing");
+    } else {
+        displayAlert("please enter value", "danger");
+    }
+}
+
+function displayAlert (text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 2000);
+}
+
+
+function setBackToDefault() {
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
+};
 
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+    console.log("added to local storage");
+};
 
 // ****** SETUP ITEMS **********
