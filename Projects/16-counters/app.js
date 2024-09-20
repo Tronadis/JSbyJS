ffb3497a6dc3517409ce3df356a6d44b55bfb3e1
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-09-20 15:48:57 +0200

    Counters basic shaping

--- Projects/16-counters/app.js
+++ Projects/16-counters/app.js
@@ -1,1 +1,20 @@
-// 16-counters OOP project setup
+function getElement(selection) {
+	const element = document.querySelector(selection);
+	if (element) {
+		return element;
+	}
+	throw new Error(`The selector "${selection}" seems incorrect.`);
+}
+
+function Counter(element, value) {
+	this.counter = element;
+	this.value = value;
+	this.decreaseBtn = element.querySelector('.decrease');
+	this.resetBtn = element.querySelector('.reset');
+	this.increaseBtn = element.querySelector('.increase');
+	this.valueSpan = element.querySelector('.value');
+	this.valueSpan.textContent = this.value;
+}
+
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
