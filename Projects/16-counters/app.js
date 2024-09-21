192e4f29e071a378610829ad04ae83ceb93cfede
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-09-21 18:00:29 +0200

    Counters complete.

--- Projects/16-counters/app.js
+++ Projects/16-counters/app.js
@@ -14,7 +14,27 @@ function Counter(element, value) {
 	this.increaseBtn = element.querySelector('.increase');
 	this.valueSpan = element.querySelector('.value');
 	this.valueSpan.textContent = this.value;
+	// Bind current `this` to te prototype's functions:
+	this.decrease = this.decrease.bind(this);
+	this.reset = this.reset.bind(this);
+	this.increase = this.increase.bind(this);
+	// .
+	this.decreaseBtn.addEventListener('click', this.decrease);
+	this.resetBtn.addEventListener('click', this.reset);
+	this.increaseBtn.addEventListener('click', this.increase);
 }
+Counter.prototype.decrease = function () {
+	this.value--;
+	this.valueSpan.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+	this.value = 0;
+	this.valueSpan.textContent = this.value;
+};
+Counter.prototype.increase = function () {
+	this.value++;
+	this.valueSpan.textContent = this.value;
+};
 
 const firstCounter = new Counter(getElement('.first-counter'), 100);
 const secondCounter = new Counter(getElement('.second-counter'), 200);
