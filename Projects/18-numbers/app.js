df97d038d2e45b37b8a475f6d6f1607e2f3ddfd6
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-20 15:49:15 +0200

    18-numbers complete.

--- Projects/18-numbers/app.js
+++ Projects/18-numbers/app.js
@@ -1,1 +1,19 @@
-console.log('numbers project');
+const numbers = document.getElementsByClassName('number');
+
+const updateCount = (el) => {
+	const value = parseInt(el.dataset.value, 10);
+	const increment = Math.ceil(value / 1000);
+	let initialValue = 0;
+
+	const increaseCount = setInterval(() => {
+		initialValue += increment;
+		if (initialValue > value) {
+			el.textContent = `${value}+`;
+			clearInterval(increaseCount);
+			return;
+		}
+		el.textContent = `${initialValue}+`;
+	}, 1);
+};
+
+[...numbers].forEach((number) => updateCount(number));
