718e12d5d2fd2b6393cabcf8dc50dce4187a8471
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-21 16:02:07 +0200

    toggle dark mode

--- Projects/19-dark-mode/app.js
+++ Projects/19-dark-mode/app.js
@@ -1,1 +1,21 @@
 console.log('dark mode');
+
+const btn = document.querySelector('.btn');
+
+// my attempt (works):
+/* const html = document.querySelector('html');
+toggleTheme = () => {
+	if (html.classList.contains('my-dark-theme')) {
+		html.classList.remove('my-dark-theme');
+	} else {
+		html.classList.add('my-dark-theme');
+	}
+};
+btn.addEventListener('click', toggleTheme); */
+
+// Teacher's straighter way:
+btn.addEventListener('click', () => {
+	document.documentElement.classList.toggle('dark-theme');
+});
+/* — `documentElelement` = the <html> tag 
+   — `toggle` is a shorthand for that if else logic */
