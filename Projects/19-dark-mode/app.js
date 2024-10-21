3ae841c3e95b1e60948ace0ede3e927da9bb602f
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-21 17:12:22 +0200

    import and display data

--- Projects/19-dark-mode/app.js
+++ Projects/19-dark-mode/app.js
@@ -1,5 +1,6 @@
 console.log('dark mode');
 
+// DARK MODE:
 const btn = document.querySelector('.btn');
 
 // my attempt (works):
@@ -19,3 +20,21 @@ btn.addEventListener('click', () => {
 });
 /* — `documentElelement` = the <html> tag 
    — `toggle` is a shorthand for that if else logic */
+
+// IMPORT DATA:
+const articlesContainer = document.querySelector('.articles');
+const articlesData = articles
+	.map((article) => {
+		console.log(article);
+		const { title, date, length, snippet } = article;
+		// TODO: format the date
+		return `
+			<article class="post">
+				<h2>${title}</h2>
+				<div class="post-info"><span>${date}</span><span>${length}</span></div>
+				<p>${snippet}</p>
+			</article>
+	`;
+	})
+	.join('');
+articlesContainer.innerHTML = articlesData;
