332100eac3e890c3cee60b65a294e3980786b3a9
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-22 17:20:51 +0200

    display products

--- Projects/20-filters/app.js
+++ Projects/20-filters/app.js
@@ -1,1 +1,24 @@
 console.log('filters project');
+
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+	// TODO: if statement "sorry..."
+	productsContainer.innerHTML = filteredProducts
+		.map((product) => {
+			const { id, title, image, company, price } = product;
+			return `
+			<article class="product" data-id="${id}">
+					<img src="${image}" class="product-img img" />
+					<footer>
+						<h5 class="product-name">${title}</h5>
+						<span class="product-price">${price}</span>
+					</footer>
+				</article>
+	`;
+		})
+		.join('');
+};
+displayProducts();
