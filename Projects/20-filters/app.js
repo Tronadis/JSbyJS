9d65021763d7dd3fa794b0217dd4335f513f7efb
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-23 15:54:51 +0200

    add error message for empty search result

--- Projects/20-filters/app.js
+++ Projects/20-filters/app.js
@@ -5,12 +5,16 @@ let filteredProducts = [...products];
 const productsContainer = document.querySelector('.products-container');
 
 const displayProducts = () => {
-	// TODO: if statement "sorry..."
-	productsContainer.innerHTML = filteredProducts
-		.map((product) => {
-			const { id, title, image, company, price } = product;
-			return `
-			<article class="product" data-id="${id}">
+	if (filteredProducts.length < 1) {
+		productsContainer.innerHTML = `
+			<h6>Sorry, we don't have this. Try someting else :)</h6>
+		`;
+	} else {
+		productsContainer.innerHTML = filteredProducts
+			.map((product) => {
+				const { id, title, image, company, price } = product;
+				return `
+				<article class="product" data-id="${id}">
 					<img src="${image}" class="product-img img" />
 					<footer>
 						<h5 class="product-name">${title}</h5>
@@ -18,8 +22,9 @@ const displayProducts = () => {
 					</footer>
 				</article>
 	`;
-		})
-		.join('');
+			})
+			.join('');
+	}
 };
 displayProducts();
 
