9250df239a40a7d76e07fb2e07c10e97788d77d3
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-29 14:48:03 +0100

    AJAX with fetch, async/await, and try/catch

--- JavaScript-tutorial/AJAX/app.js
+++ JavaScript-tutorial/AJAX/app.js
@@ -1,5 +1,6 @@
 const textBtn = document.querySelector('.btn-text');
 const jsonBtn = document.querySelector('.btn-JSON');
+const fetchBtn = document.querySelector('.fetch-JSON');
 const textUrl = './API/sample.txt';
 const jsonUrl = './API/people.json';
 const xhr = new XMLHttpRequest();
@@ -61,3 +62,37 @@ const getJsonData = (url) => {
 
 textBtn.addEventListener('click', () => getData(textUrl));
 jsonBtn.addEventListener('click', () => getJsonData(jsonUrl));
+
+// fetch:
+
+// - without async/await
+/* fetchBtn.addEventListener('click', () => {
+	fetch(jsonUrl)
+		.then((resp) => resp.json())
+		.then((data) => {
+			displayItems(data);
+		})
+		.catch((err) => console.log(err));
+}); */
+
+// - with async/await
+fetchBtn.addEventListener('click', async () => {
+	const response = await fetch(jsonUrl);
+	const data = await response.json();
+	displayItems(data);
+});
+
+const displayItems = (items) => {
+	const displayData = items
+		.map((item) => {
+			const { name } = item; // This is the same as `const name = item.name`, but using object destructuring.
+			return `<p>${name}</p>`;
+		})
+		.join('');
+	const div = document.createElement('div');
+	div.innerHTML = displayData;
+	document.body.appendChild(div);
+};
+/* Note: `const` and arrow functions still cannot be hoisted, 
+	but in this case since `fetch` is asynchronous, 
+	`displayItems` is read before the button can be clicked :)  */
