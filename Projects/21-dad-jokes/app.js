69e838bfad702ff41ad44f810c69001915954d87
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-30 17:42:06 +0100

    fetch from API

--- Projects/21-dad-jokes/app.js
+++ Projects/21-dad-jokes/app.js
@@ -1,1 +1,21 @@
 console.log('Dad Jokes Project');
+
+const url = 'https://icanhazdadjoke.com/';
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => fetchDadJoke());
+
+const fetchDadJoke = async () => {
+	result.textContent = 'Loading...';
+	const response = await fetch(url, {
+		headers: {
+			Accept: 'application/json',
+			'User-Agent': 'learning-app',
+		},
+	});
+	const data = await response.json();
+	result.textContent = data.joke;
+};
+
+fetchDadJoke(); // fetch right away on page load
