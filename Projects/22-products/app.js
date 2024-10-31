95f757871d7045f8894dbcbb4eb39ea1e0f93104
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-31 21:10:12 +0100

    display products

--- Projects/22-products/app.js
+++ Projects/22-products/app.js
@@ -9,11 +9,39 @@ const fetchProducts = async () => {
 	try {
 		const resp = await fetch(url);
 		const data = await resp.json();
-		console.log(data);
+		return data;
 	} catch (error) {
 		productsDOM.innerHTML = `<p class="error">There was an error.</p>`;
 		console.log(error);
 	}
 };
+const displayProducts = (list) => {
+	const productsList = list
+		.map((product) => {
+			const { id } = product;
+			const { name: title, price } = product.fields;
+			const { url: img } = product.fields.image[0];
+			const formatPrice = price / 100;
+			// console.log(img);
+			// id, name, price, img
+			return `<a href="./product.html?id=${id}" class="single-product">
+						<img src="${img}" alt="${title}" class="single-product-img img" />
+						<footer>
+							<h5 class="name">${title}</h5>
+							<span class="price">$ ${formatPrice}</span>
+						</footer>
+					</a>`;
+		})
+		.join('');
+	productsDOM.innerHTML = `<div class="products-container">
+					${productsList}
+				</div>`;
+};
+
+const start = async () => {
+	const data = await fetchProducts();
+	displayProducts(data);
+};
+start();
 
 fetchProducts();
