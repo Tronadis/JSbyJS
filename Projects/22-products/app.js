c7404d22d8121a30a00eda7297c0ff0481cccc62
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-10-31 19:33:32 +0100

    fetch

--- Projects/22-products/app.js
+++ Projects/22-products/app.js
@@ -1,1 +1,19 @@
 console.log('products project');
+
+const url = 'https://www.course-api.com/javascript-store-products';
+
+const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+	productsDOM.innerHTML = `<div class="loading"></div>`;
+	try {
+		const resp = await fetch(url);
+		const data = await resp.json();
+		console.log(data);
+	} catch (error) {
+		productsDOM.innerHTML = `<p class="error">There was an error.</p>`;
+		console.log(error);
+	}
+};
+
+fetchProducts();
