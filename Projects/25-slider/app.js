80c73d9b22692ac3c869fc27577d87bb001ae51e
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-08 16:19:13 +0100

    slider prev and next

--- Projects/25-slider/app.js
+++ Projects/25-slider/app.js
@@ -9,7 +9,7 @@ const nextBtn = document.querySelector('.next-btn');
 container.innerHTML = people
 	.map((person, slideIndex) => {
 		const { img, name, job, text } = person;
-		let position = 'next';
+		let position = '';
 		if (slideIndex === 0) position = 'active';
 		if (slideIndex === people.length - 1) position = 'last';
 		return `<article class="slide ${position}">
@@ -23,3 +23,29 @@ container.innerHTML = people
 		        </article>`;
 	})
 	.join('');
+
+const startSlider = (type) => {
+	const active = document.querySelector('.active');
+	const last = document.querySelector('.last');
+	let next = active.nextElementSibling;
+	if (!next) next = container.firstElementChild;
+
+	active.classList.remove('active');
+	last.classList.remove('last');
+	next.classList.remove('next');
+
+	if (type === 'prev') {
+		active.classList.add('next');
+		last.classList.add('active');
+		next = last.previousElementSibling;
+		if (!next) next = container.lastElementChild;
+		next.classList.add('last');
+		return;
+	}
+
+	active.classList.add('last');
+	last.classList.add('next');
+	next.classList.add('active');
+};
+nextBtn.addEventListener('click', () => startSlider());
+prevBtn.addEventListener('click', () => startSlider('prev'));
--- Projects/25-slider/data.js
+++ Projects/25-slider/data.js
@@ -17,6 +17,42 @@ const people = [
 		job: 'designer',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
 	},
+	{
+		img: 'https://www.course-api.com/images/people/person-3.jpeg',
+		name: 'peter doe 2',
+		job: 'product manager',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+	},
+	{
+		img: 'https://www.course-api.com/images/people/person-1.jpeg',
+		name: 'susan doe 2',
+		job: 'developer',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
+	{
+		img: 'https://www.course-api.com/images/people/person-2.jpeg',
+		name: 'emma doe 2',
+		job: 'designer',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
+	{
+		img: 'https://www.course-api.com/images/people/person-3.jpeg',
+		name: 'peter doe 3',
+		job: 'product manager',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+	},
+	{
+		img: 'https://www.course-api.com/images/people/person-1.jpeg',
+		name: 'susan doe 3',
+		job: 'developer',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
+	{
+		img: 'https://www.course-api.com/images/people/person-2.jpeg',
+		name: 'emma doe 3',
+		job: 'designer',
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
 ];
 
 export default people;
