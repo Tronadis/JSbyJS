df50d367e6e314879065e457810e586070dde5ec
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-09 20:47:00 +0100

    sidebar, with dynamic contents

--- Projects/26-stripe-submenus/app.js
+++ Projects/26-stripe-submenus/app.js
@@ -1,1 +1,37 @@
 console.log('stripe submenus');
+
+import sublinks from './data.js';
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebarLinks = document.querySelector('.sidebar-links');
+// const linkBtns = [...document.querySelectorAll('.link-btn')];
+// const submenu = document.querySelector('.submenu');
+// const hero = document.querySelector('.hero');
+// const nav = document.querySelector('.nav');
+
+toggleBtn.addEventListener('click', () => {
+	sidebarWrapper.classList.toggle('show');
+});
+closeBtn.addEventListener('click', () => {
+	sidebarWrapper.classList.remove('show');
+});
+
+sidebarLinks.innerHTML = sublinks
+	.map((item) => {
+		const { page, links } = item;
+		return `
+            <article>
+				<h4>${page}</h4>
+				<div class="sidebar-sublinks">${links
+					.map(
+						(link) =>
+							`<a href="${link.url}">
+                                <i class="${link.icon}"></i>${link.label}
+                            </a>`
+					)
+					.join('')}</div>
+			</article>`;
+	})
+	.join('');
