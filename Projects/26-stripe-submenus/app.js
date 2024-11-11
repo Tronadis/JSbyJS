4ed257a8693b306e1d40ce12281659d1aaec0dc7
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-11 16:15:12 +0100

    submenu element with dynamic placing

--- Projects/26-stripe-submenus/app.js
+++ Projects/26-stripe-submenus/app.js
@@ -6,8 +6,8 @@ const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
 const sidebarWrapper = document.querySelector('.sidebar-wrapper');
 const sidebarLinks = document.querySelector('.sidebar-links');
-// const linkBtns = [...document.querySelectorAll('.link-btn')];
-// const submenu = document.querySelector('.submenu');
+const linkBtns = [...document.querySelectorAll('.link-btn')];
+const submenu = document.querySelector('.submenu');
 // const hero = document.querySelector('.hero');
 // const nav = document.querySelector('.nav');
 
@@ -21,11 +21,23 @@ closeBtn.addEventListener('click', () => {
 sidebarLinks.innerHTML = sublinks
 	.map((item) => {
 		const { page, links } = item;
-		return html` <article>
+		return /* html */ `<article>
 			<h4>${page}</h4>
 			<div class="sidebar-sublinks">
-				${links.map((link) => html`<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`).join('')}
+				${links.map((link) => /* html */ `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`).join('')}
 			</div>
 		</article>`;
 	})
 	.join('');
+
+linkBtns.forEach((btn) => {
+	btn.addEventListener('mouseover', function (e) {
+		const text = e.currentTarget.textContent;
+		const tempBtn = e.currentTarget.getBoundingClientRect();
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
+		submenu.style.left = `${center}px`;
+		submenu.style.top = `${bottom}px`;
+		submenu.classList.add('show');
+	});
+});
