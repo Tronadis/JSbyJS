204990434230b6a835cd56dd06e5270dd50aeaf0
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-12 13:17:21 +0100

    fetch data and display it

--- Projects/27-pagination/app.js
+++ Projects/27-pagination/app.js
@@ -2,3 +2,13 @@ import fetchFollowers from './fetchFollowers.js';
 import displayFollowers from './displayFollowers.js';
 import paginate from './paginate.js';
 import displayButtons from './displayButtons.js';
+
+const title = document.querySelector('.section-title h1');
+
+const init = async () => {
+	const followers = await fetchFollowers();
+	displayFollowers(followers);
+	title.textContent = 'Pagination';
+};
+
+window.addEventListener('load', init);
--- Projects/27-pagination/displayFollowers.js
+++ Projects/27-pagination/displayFollowers.js
@@ -1,3 +1,18 @@
-const display = () => {};
+const container = document.querySelector('.container');
+
+const display = (followers) => {
+	const users = followers
+		.map((person) => {
+			const { login, html_url, avatar_url } = person;
+			return /* html */ `
+        <article class="card">
+            <img src="${avatar_url}" alt="${login}">
+            <h4>${login}</h4>
+            <a href="${html_url}" class="btn">view profile</a>
+        </article>`;
+		})
+		.join('');
+	container.innerHTML = users;
+};
 
 export default display;
