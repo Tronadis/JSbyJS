46215f3c98b4e921aabe1d460a9fe27e72fe0570
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-12 17:35:44 +0100

    add navigation UI

--- Projects/27-pagination/app.js
+++ Projects/27-pagination/app.js
@@ -4,12 +4,36 @@ import paginate from './paginate.js';
 import displayButtons from './displayButtons.js';
 
 const title = document.querySelector('.section-title h1');
+const btnContainer = document.querySelector('.btn-container');
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+	displayFollowers(pages[index]);
+	displayButtons(btnContainer, pages, index);
+};
+
+btnContainer.addEventListener('click', (e) => {
+	if (e.target.classList.contains('btn-container')) return;
+	if (e.target.classList.contains('page-btn')) index = parseInt(e.target.dataset.index);
+	if (e.target.classList.contains('next-btn')) {
+		index++;
+		if (index > pages.length - 1) index = 0;
+	}
+	if (e.target.classList.contains('prev-btn')) {
+		index--;
+		if (index < pages.length - 1) index = pages.length;
+	}
+
+	setupUI();
+});
 
 const init = async () => {
 	const followers = await fetchFollowers();
-	displayFollowers(paginate(followers)[0]);
 	title.textContent = 'Pagination';
-	const pages = paginate(followers);
+	pages = paginate(followers);
+	setupUI();
 };
 
 window.addEventListener('load', init);
--- Projects/27-pagination/displayButtons.js
+++ Projects/27-pagination/displayButtons.js
@@ -1,3 +1,13 @@
-const displayButtons = () => {};
+const displayButtons = (btnContainer, pages, activeIndex) => {
+	let btns = pages.map((_, pageIndex) => {
+		return /* html*/ `
+        <button class="page-btn ${activeIndex === pageIndex ? 'active-btn' : 'null'}" data-index="${pageIndex}">${
+			pageIndex + 1
+		}</button>`;
+	});
+	btns.push(/*html*/ `<button class="next-btn">next</button>`);
+	btns.unshift(/*html*/ `<button class="prev-btn">prev</button>`);
+	btnContainer.innerHTML = btns.join('');
+};
 
 export default displayButtons;
