58a3566fb1539ec62cfe7b32e013c5f7db9b8855
Tronadis <89267750+Tronadis@users.noreply.github.com>
2024-11-13 14:24:27 +0100

    fetch search results (and handle errors)

--- Projects/28-search-wikipedia/app.js
+++ Projects/28-search-wikipedia/app.js
@@ -1,3 +1,30 @@
 console.log('wiki search');
 
 const url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
+
+const formDOM = document.querySelector('.form');
+const inputDOM = document.querySelector('.form-input');
+const resultsDOM = document.querySelector('.results');
+
+formDOM.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const value = inputDOM.value;
+	value.textContent = ''; // for Firefox
+	if (!value) resultsDOM.innerHTML = /*html*/ `<div class="error">Please write something.</div>`;
+	fetchPages(value);
+});
+const fetchPages = async (searchValue) => {
+	resultsDOM.innerHTML = /*html*/ `<div class="loading"></div>`;
+	try {
+		const response = await fetch(`${url}${searchValue}`);
+		const data = await response.json();
+		const results = data.query.search;
+		if (results.length < 1) resultsDOM.innerHTML = /*html*/ `<div class="error">Sorry, no matching result :(</div>`;
+		renderResults(results);
+	} catch (error) {
+		resultsDOM.innerHTML = /*html*/ `<div class="error">There was an error while fetching your search :(</div>`;
+	}
+};
+const renderResults = (list) => {
+	console.log(list); // TEMP
+};
